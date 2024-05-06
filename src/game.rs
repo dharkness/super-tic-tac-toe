368642acb137a01dc21a9fe672875fc6960bc816
David Harkness <david.harkness@realogy.com>
2024-05-06 00:53:16 -0700

    Rename and refactor in preparation for computer play

--- src/game.rs
+++ src/game.rs
@@ -1,27 +1,29 @@
+use std::str::FromStr;
+
 #[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
-pub enum Player {
+pub enum Mark {
     X,
     O,
 }
 
-impl Player {
-    pub fn to_string(self) -> &'static str {
+impl Mark {
+    pub fn to_str(self) -> &'static str {
         match self {
-            Player::X => "X",
-            Player::O => "O",
+            Mark::X => "X",
+            Mark::O => "O",
         }
     }
 
-    pub fn opponent(self) -> Player {
+    pub fn opponent(self) -> Mark {
         match self {
-            Player::X => Player::O,
-            Player::O => Player::X,
+            Mark::X => Mark::O,
+            Mark::O => Mark::X,
         }
     }
 }
 
 #[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
-pub enum Location {
+pub enum Coord {
     TopLeft,
     Top,
     TopRight,
@@ -33,57 +35,61 @@ pub enum Location {
     BottomRight,
 }
 
-impl Location {
-    pub fn from_index(index: usize) -> Option<Location> {
+impl Coord {
+    pub fn from_index(index: usize) -> Option<Coord> {
         match index {
-            0 => Some(Location::TopLeft),
-            1 => Some(Location::Top),
-            2 => Some(Location::TopRight),
-            3 => Some(Location::Left),
-            4 => Some(Location::Middle),
-            5 => Some(Location::Right),
-            6 => Some(Location::BottomLeft),
-            7 => Some(Location::Bottom),
-            8 => Some(Location::BottomRight),
+            0 => Some(Coord::TopLeft),
+            1 => Some(Coord::Top),
+            2 => Some(Coord::TopRight),
+            3 => Some(Coord::Left),
+            4 => Some(Coord::Middle),
+            5 => Some(Coord::Right),
+            6 => Some(Coord::BottomLeft),
+            7 => Some(Coord::Bottom),
+            8 => Some(Coord::BottomRight),
             _ => None,
         }
     }
 
-    pub fn from_input(input: &str) -> Option<Location> {
-        match input {
-            "tl" => Some(Location::TopLeft),
-            "t" => Some(Location::Top),
-            "tr" => Some(Location::TopRight),
-            "l" => Some(Location::Left),
-            "m" => Some(Location::Middle),
-            "r" => Some(Location::Right),
-            "bl" => Some(Location::BottomLeft),
-            "b" => Some(Location::Bottom),
-            "br" => Some(Location::BottomRight),
-            _ => None,
-        }
-    }
-
-    pub fn to_string(self) -> &'static str {
+    pub fn to_str(self) -> &'static str {
         match self {
-            Location::TopLeft => "Top Left",
-            Location::Top => "Top",
-            Location::TopRight => "Top Right",
-            Location::Left => "Left",
-            Location::Middle => "Middle",
-            Location::Right => "Right",
-            Location::BottomLeft => "Bottom Left",
-            Location::Bottom => "Bottom",
-            Location::BottomRight => "Bottom Right",
+            Coord::TopLeft => "top-left",
+            Coord::Top => "top",
+            Coord::TopRight => "top-right",
+            Coord::Left => "left",
+            Coord::Middle => "middle",
+            Coord::Right => "right",
+            Coord::BottomLeft => "bottom-left",
+            Coord::Bottom => "bottom",
+            Coord::BottomRight => "bottom-right",
+        }
+    }
+}
+
+impl FromStr for Coord {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Coord, ()> {
+        match input {
+            "tl" => Ok(Coord::TopLeft),
+            "t" => Ok(Coord::Top),
+            "tr" => Ok(Coord::TopRight),
+            "l" => Ok(Coord::Left),
+            "m" => Ok(Coord::Middle),
+            "r" => Ok(Coord::Right),
+            "bl" => Ok(Coord::BottomLeft),
+            "b" => Ok(Coord::Bottom),
+            "br" => Ok(Coord::BottomRight),
+            _ => Err(()),
         }
     }
 }
 
 #[derive(Clone, Copy, Debug, Default, Hash, Eq, PartialEq)]
-pub struct Cell(Option<Player>);
+pub struct Cell(Option<Mark>);
 
 impl Cell {
-    pub fn player(&self) -> Option<Player> {
+    pub fn mark(&self) -> Option<Mark> {
         self.0
     }
 
@@ -91,36 +97,36 @@ impl Cell {
         self.0.is_none()
     }
 
-    pub fn place_move(&mut self, player: Player) -> bool {
+    pub fn place_move(&mut self, mark: Mark) -> bool {
         if self.is_empty() {
-            self.0 = Some(player);
+            self.0 = Some(mark);
             true
         } else {
             false
         }
     }
 
-    pub fn to_string(self) -> &'static str{
+    pub fn to_str(self) -> &'static str{
         match self.0 {
             None => " ",
-            Some(player) => player.to_string(),
+            Some(mark) => mark.to_str(),
         }
     }
 }
 
 #[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
-pub struct Game {
-    boards: [Board; 9],
-    winner: Option<Player>,
+pub struct Board {
+    boards: [InnerBoard; 9],
+    winner: Option<Mark>,
 }
 
-impl Game {
-    pub fn new() -> Game {
-        Game { boards: [Board::new(); 9], winner: None }
+impl Board {
+    pub fn new() -> Board {
+        Board { boards: [InnerBoard::new(); 9], winner: None }
     }
 
-    pub fn place_move(&mut self, player: Player, board: Location, cell: Location) -> bool {
-        if self.boards[board as usize].place_move(player, cell) {
+    pub fn place_move(&mut self, mark: Mark, board: Coord, cell: Coord) -> bool {
+        if self.boards[board as usize].place_move(mark, cell) {
             if self.boards[board as usize].winner().is_some() {
                 self.winner = self.find_winner();
             }
@@ -130,20 +136,20 @@ impl Game {
         }
     }
 
-    pub fn winner(&self) -> Option<Player> {
+    pub fn winner(&self) -> Option<Mark> {
         self.winner
     }
 
-    pub fn find_winner(&self) -> Option<Player> {
+    pub fn find_winner(&self) -> Option<Mark> {
         let winning_combinations = [
-            [Location::TopLeft, Location::Top, Location::TopRight],
-            [Location::Left, Location::Middle, Location::Right],
-            [Location::BottomLeft, Location::Bottom, Location::BottomRight],
-            [Location::TopLeft, Location::Left, Location::BottomLeft],
-            [Location::Top, Location::Middle, Location::Bottom],
-            [Location::TopRight, Location::Right, Location::BottomRight],
-            [Location::TopLeft, Location::Middle, Location::BottomRight],
-            [Location::TopRight, Location::Middle, Location::BottomLeft],
+            [Coord::TopLeft, Coord::Top, Coord::TopRight],
+            [Coord::Left, Coord::Middle, Coord::Right],
+            [Coord::BottomLeft, Coord::Bottom, Coord::BottomRight],
+            [Coord::TopLeft, Coord::Left, Coord::BottomLeft],
+            [Coord::Top, Coord::Middle, Coord::Bottom],
+            [Coord::TopRight, Coord::Right, Coord::BottomRight],
+            [Coord::TopLeft, Coord::Middle, Coord::BottomRight],
+            [Coord::TopRight, Coord::Middle, Coord::BottomLeft],
         ];
 
         for combination in winning_combinations.iter() {
@@ -161,36 +167,36 @@ impl Game {
         None
     }
 
-    pub fn board_is_won(&self, board: Location) -> bool {
+    pub fn board_is_won(&self, board: Coord) -> bool {
         self.boards[board as usize].winner().is_some()
     }
 
     pub fn lines(&self) -> [String; 23] {
         let boards = self.boards.iter().map(|board| board.lines()).collect::<Vec<_>>();
         [
-            "       ║       ║       ".to_string(),
-            format!(" {} ║ {} ║ {} ", boards[0][0], boards[1][0], boards[2][0]),
-            format!(" {} ║ {} ║ {} ", boards[0][1], boards[1][1], boards[2][1]),
-            format!(" {} ║ {} ║ {} ", boards[0][2], boards[1][2], boards[2][2]),
-            format!(" {} ║ {} ║ {} ", boards[0][3], boards[1][3], boards[2][3]),
-            format!(" {} ║ {} ║ {} ", boards[0][4], boards[1][4], boards[2][4]),
-            "       ║       ║       ".to_string(),
-            "═══════╬═══════╬═══════".to_string(),
-            "       ║       ║       ".to_string(),
-            format!(" {} ║ {} ║ {} ", boards[3][0], boards[4][0], boards[5][0]),
-            format!(" {} ║ {} ║ {} ", boards[3][1], boards[4][1], boards[5][1]),
-            format!(" {} ║ {} ║ {} ", boards[3][2], boards[4][2], boards[5][2]),
-            format!(" {} ║ {} ║ {} ", boards[3][3], boards[4][3], boards[5][3]),
-            format!(" {} ║ {} ║ {} ", boards[3][4], boards[4][4], boards[5][4]),
-            "       ║       ║       ".to_string(),
-            "═══════╬═══════╬═══════".to_string(),
-            "       ║       ║       ".to_string(),
-            format!(" {} ║ {} ║ {} ", boards[6][0], boards[7][0], boards[8][0]),
-            format!(" {} ║ {} ║ {} ", boards[6][1], boards[7][1], boards[8][1]),
-            format!(" {} ║ {} ║ {} ", boards[6][2], boards[7][2], boards[8][2]),
-            format!(" {} ║ {} ║ {} ", boards[6][3], boards[7][3], boards[8][3]),
-            format!(" {} ║ {} ║ {} ", boards[6][4], boards[7][4], boards[8][4]),
-            "       ║       ║       ".to_string(),
+            "       ┃       ┃       ".to_string(),
+            format!(" {} ┃ {} ┃ {} ", boards[0][0], boards[1][0], boards[2][0]),
+            format!(" {} ┃ {} ┃ {} ", boards[0][1], boards[1][1], boards[2][1]),
+            format!(" {} ┃ {} ┃ {} ", boards[0][2], boards[1][2], boards[2][2]),
+            format!(" {} ┃ {} ┃ {} ", boards[0][3], boards[1][3], boards[2][3]),
+            format!(" {} ┃ {} ┃ {} ", boards[0][4], boards[1][4], boards[2][4]),
+            "       ┃       ┃       ".to_string(),
+            "━━━━━━━╋━━━━━━━╋━━━━━━━".to_string(),
+            "       ┃       ┃       ".to_string(),
+            format!(" {} ┃ {} ┃ {} ", boards[3][0], boards[4][0], boards[5][0]),
+            format!(" {} ┃ {} ┃ {} ", boards[3][1], boards[4][1], boards[5][1]),
+            format!(" {} ┃ {} ┃ {} ", boards[3][2], boards[4][2], boards[5][2]),
+            format!(" {} ┃ {} ┃ {} ", boards[3][3], boards[4][3], boards[5][3]),
+            format!(" {} ┃ {} ┃ {} ", boards[3][4], boards[4][4], boards[5][4]),
+            "       ┃       ┃       ".to_string(),
+            "━━━━━━━╋━━━━━━━╋━━━━━━━".to_string(),
+            "       ┃       ┃       ".to_string(),
+            format!(" {} ┃ {} ┃ {} ", boards[6][0], boards[7][0], boards[8][0]),
+            format!(" {} ┃ {} ┃ {} ", boards[6][1], boards[7][1], boards[8][1]),
+            format!(" {} ┃ {} ┃ {} ", boards[6][2], boards[7][2], boards[8][2]),
+            format!(" {} ┃ {} ┃ {} ", boards[6][3], boards[7][3], boards[8][3]),
+            format!(" {} ┃ {} ┃ {} ", boards[6][4], boards[7][4], boards[8][4]),
+            "       ┃       ┃       ".to_string(),
         ]
     }
 
@@ -202,22 +208,22 @@ impl Game {
 }
 
 #[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
-pub struct Board {
+pub struct InnerBoard {
     cells: [Cell; 9],
-    winner: Option<Player>,
+    winner: Option<Mark>,
 }
 
-impl Board {
-    pub fn new() -> Board {
-        Board { cells: [Cell::default(); 9], winner: None }
+impl InnerBoard {
+    pub fn new() -> InnerBoard {
+        InnerBoard { cells: [Cell::default(); 9], winner: None }
     }
 
-    pub fn is_empty(&self, cell: Location) -> bool {
+    pub fn is_empty(&self, cell: Coord) -> bool {
         self.cells[cell as usize].is_empty()
     }
 
-    pub fn place_move(&mut self, player: Player, cell: Location) -> bool {
-        if self.winner.is_none() && self.cells[cell as usize].place_move(player) {
+    pub fn place_move(&mut self, mark: Mark, cell: Coord) -> bool {
+        if self.winner.is_none() && self.cells[cell as usize].place_move(mark) {
             self.winner = self.find_winner();
             true
         } else {
@@ -225,20 +231,20 @@ impl Board {
         }
     }
 
-    pub fn winner(&self) -> Option<Player> {
+    pub fn winner(&self) -> Option<Mark> {
         self.winner
     }
 
-    pub fn find_winner(&self) -> Option<Player> {
+    pub fn find_winner(&self) -> Option<Mark> {
         let winning_combinations = [
-            [Location::TopLeft, Location::Top, Location::TopRight],
-            [Location::Left, Location::Middle, Location::Right],
-            [Location::BottomLeft, Location::Bottom, Location::BottomRight],
-            [Location::TopLeft, Location::Left, Location::BottomLeft],
-            [Location::Top, Location::Middle, Location::Bottom],
-            [Location::TopRight, Location::Right, Location::BottomRight],
-            [Location::TopLeft, Location::Middle, Location::BottomRight],
-            [Location::TopRight, Location::Middle, Location::BottomLeft],
+            [Coord::TopLeft, Coord::Top, Coord::TopRight],
+            [Coord::Left, Coord::Middle, Coord::Right],
+            [Coord::BottomLeft, Coord::Bottom, Coord::BottomRight],
+            [Coord::TopLeft, Coord::Left, Coord::BottomLeft],
+            [Coord::Top, Coord::Middle, Coord::Bottom],
+            [Coord::TopRight, Coord::Right, Coord::BottomRight],
+            [Coord::TopLeft, Coord::Middle, Coord::BottomRight],
+            [Coord::TopRight, Coord::Middle, Coord::BottomLeft],
         ];
 
         for combination in winning_combinations.iter() {
@@ -249,7 +255,7 @@ impl Board {
             ];
 
             if !cells[0].is_empty() && cells[0] == cells[1] && cells[1] == cells[2] {
-                return cells[0].player();
+                return cells[0].mark();
             }
         }
 
@@ -259,20 +265,20 @@ impl Board {
     pub fn lines(&self) -> [String; 5] {
         match self.winner {
             None => [
-                format!("{}│{}│{}", self.cells[0].to_string(), self.cells[1].to_string(), self.cells[2].to_string()),
+                format!("{}│{}│{}", self.cells[0].to_str(), self.cells[1].to_str(), self.cells[2].to_str()),
                 "─┼─┼─".to_string(),
-                format!("{}│{}│{}", self.cells[3].to_string(), self.cells[4].to_string(), self.cells[5].to_string()),
+                format!("{}│{}│{}", self.cells[3].to_str(), self.cells[4].to_str(), self.cells[5].to_str()),
                 "─┼─┼─".to_string(),
-                format!("{}│{}│{}", self.cells[6].to_string(), self.cells[7].to_string(), self.cells[8].to_string()),
+                format!("{}│{}│{}", self.cells[6].to_str(), self.cells[7].to_str(), self.cells[8].to_str()),
             ],
-            Some(Player::X) => [
+            Some(Mark::X) => [
                 "X   X".to_string(),
                 " X X ".to_string(),
                 "  X  ".to_string(),
                 " X X ".to_string(),
                 "X   X".to_string(),
             ],
-            Some(Player::O) => [
+            Some(Mark::O) => [
                 " OOO ".to_string(),
                 "O   O".to_string(),
                 "O   O".to_string(),
@@ -280,13 +286,5 @@ impl Board {
                 " OOO ".to_string(),
             ],
         }
-    }
-
-    pub fn print(&self) {
-        println!("{}|{}|{}", self.cells[0].to_string(), self.cells[1].to_string(), self.cells[2].to_string());
-        println!("-+-+-");
-        println!("{}|{}|{}", self.cells[3].to_string(), self.cells[4].to_string(), self.cells[5].to_string());
-        println!("-+-+-");
-        println!("{}|{}|{}", self.cells[6].to_string(), self.cells[7].to_string(), self.cells[8].to_string());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,61 +6,63 @@
 )]
 
 use std::io;
-use crate::game::{Game, Location, Player};
+use std::str::FromStr;
+
+use crate::game::{Board, Coord, Mark};
 
 mod game;
 
 fn main() {
-    let mut game = Game::new();
-    let mut player = Player::X;
-    let mut board: Option<Location> = None;
+    let mut game = Board::new();
+    let mut mark = Mark::X;
+    let mut outer: Option<Coord> = None;
 
     while game.winner().is_none() {
         println!();
         game.print();
-        println!("\n{}'s turn\n", player.to_string());
+        println!("\n{}'s turn\n", mark.to_str());
 
-        if board.is_none() {
+        if outer.is_none() {
             println!("Enter the board location:");
             let mut input_text = String::new();
             io::stdin()
                 .read_line(&mut input_text)
                 .expect("failed to read from stdin");
 
-            board = Location::from_input(input_text.trim().to_lowercase().as_str());
-            if board.is_none() {
+            outer = Coord::from_str(input_text.trim().to_lowercase().as_str()).ok();
+            if outer.is_none() {
                 println!("\nInvalid board location");
                 continue;
             }
         }
 
-        println!("Enter the {} cell location:", board.unwrap().to_string());
+        println!("Enter the {} cell location:", outer.unwrap().to_str());
         let mut input_text = String::new();
         io::stdin()
             .read_line(&mut input_text)
             .expect("failed to read from stdin");
 
-        let cell = Location::from_input(input_text.trim().to_lowercase().as_str());
-        if cell.is_none() {
+        let inner = Coord::from_str(input_text.trim().to_lowercase().as_str()).ok();
+        if inner.is_none() {
             println!("\nInvalid cell location");
             continue;
         }
 
-        if game.place_move(player, board.unwrap(), cell.unwrap()) {
-            player = player.opponent();
+        if game.place_move(mark, outer.unwrap(), inner.unwrap()) {
+            mark = mark.opponent();
         } else {
             println!("\nInvalid move");
             continue;
         }
 
-        if game.board_is_won(cell.unwrap()) {
-            board = None;
+        if game.board_is_won(inner.unwrap()) {
+            outer = None;
         } else {
-            board = Some(cell.unwrap());
+            outer = Some(inner.unwrap());
         }
     }
 
-    println!("\n{} wins!\n", game.winner().unwrap().to_string());
+    println!("\n{} wins!\n", game.winner().unwrap().to_str());
 
     game.print();
 }
